0768b46e45d798cea4f78785d4af2fdf41f03165
Aditya Krisshna <adkrishna@deloitte.com>
2020-02-26 02:16:01 +0530

    Change1 Basic

---
+++ HerokuUseCaseSample/src/main/java/com/deloitte/HerokuUseCaseSample/HerokuUseCaseSampleApplication.java
@@ -0,0 +1,26 @@
+package com.deloitte.HerokuUseCaseSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HerokuUseCaseSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuUseCaseSampleApplication.class, args);
+	}
+	
+}
+//Adkrishna : Added to test Simple Spring boot 
+@RestController
+class HerokuController{
+	@GetMapping("/myWeb")
+	String hello()
+	{
+		return "My First Web App";
+	}
+}
+//Close Simple App
+
