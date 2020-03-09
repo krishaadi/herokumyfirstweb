c2aeb61bf41b36ea8ceadce283e3eab1e2760ffc
US\rrreddy <rrreddy@deloitte.com>
2020-03-09 13:39:45 +0530

    display pdf in browser

--- HerokuUseCaseSample/src/main/java/com/deloitte/HerokuUseCaseSample/HerokuUseCaseSampleApplication.java
+++ HerokuUseCaseSample/src/main/java/com/deloitte/HerokuUseCaseSample/HerokuUseCaseSampleApplication.java
@@ -2,35 +2,35 @@ package com.deloitte.HerokuUseCaseSample;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.awt.Desktop;
-import java.net.URI;
-import java.net.URL;
 
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.S3Object;
 import com.deloitte.GenPDF.DBConnection;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -61,18 +61,18 @@ public class HerokuUseCaseSampleApplication {
 // Close Simple App
 class Generate_PDF_Dynamic { // 2. A new Class for PDF Generation
 	@GetMapping(path = "/{contactId}") // 3. Get URL parameter
-	public String myPdf(@PathVariable String contactId) { // 4. Get Path variable in URL
-		
+	public ResponseEntity<InputStreamResource> myPdf(@PathVariable String contactId) { // 4. Get Path variable in URL
+
 		Document document = new Document();
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		
+
 		try { // 5. main try-catch block
 
-			String bucketName=System.getenv("BUCKET_NAME");//"gpsdemodoc";
-			String bucketKey=System.getenv("BUCKET_KEY");//"AKIAQL377ZCZDGQUCB3H";
-			String bucketSecret=System.getenv("BUCKET_SECRET");//"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";
+			String bucketName = System.getenv("BUCKET_NAME");// "gpsdemodoc";
+			String bucketKey = System.getenv("BUCKET_KEY");// "AKIAQL377ZCZDGQUCB3H";
+			String bucketSecret = System.getenv("BUCKET_SECRET");// "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";
 			String region = "us-east-2";
-			
+
 			// Composing PDF
 			try {
 				PdfWriter.getInstance(document, outputStream);
@@ -87,7 +87,7 @@ class Generate_PDF_Dynamic { // 2. A new Class for PDF Generation
 			Connection conn = app.connect();
 			PreparedStatement ps = null;
 			ResultSet rs = null;
-			String query = "Select Name FROM salesforce.contact where Sfid = '0036F000022vdzAQAQ'";///ContactId
+			String query = "Select Name FROM salesforce.contact where Sfid = '0036F000022vdzAQAQ'";/// ContactId
 			ps = conn.prepareStatement(query);
 			rs = ps.executeQuery();
 			System.out.println("Connected to DB");
@@ -122,36 +122,78 @@ class Generate_PDF_Dynamic { // 2. A new Class for PDF Generation
 
 				htmlWorker.parse(new StringReader(str));
 			}
-			
+
 			document.close();
-			
-			BasicAWSCredentials creds = new BasicAWSCredentials(bucketKey, bucketSecret); 
-			AmazonS3 s3Client = AmazonS3ClientBuilder
-											.standard()
-											.withRegion(Regions.US_EAST_2)
-											.withCredentials(new AWSStaticCredentialsProvider(creds)).build();
-			
-			ByteArrayInputStream arrayInputStream=new ByteArrayInputStream(outputStream.toByteArray());
-			
+
+			BasicAWSCredentials creds = new BasicAWSCredentials(bucketKey, bucketSecret);
+			AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2)
+					.withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+
+			ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
 			final ObjectMetadata metadata = new ObjectMetadata();
 			metadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
 			metadata.setContentType("application/pdf");
 			metadata.setContentLength(arrayInputStream.available());
-			final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, "Document/Demo-"+contactId+".pdf",
-			        new BufferedInputStream(arrayInputStream), metadata);
-			putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead); //making the object public
+			
+			String fileName = "Demo-" + contactId + ".pdf";
+			String key = "Document/" + fileName;
+			
+			
+			
+			final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key,
+					new BufferedInputStream(arrayInputStream), metadata);
+			putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead); // making the object public
 
 			final PutObjectResult result = s3Client.putObject(putObjectRequest);
+
+			System.out.println("Tag: " + result.getETag());
+
 			
-			System.out.println("Tag: "+result.getETag());
-			//Construct URL to open documen- - Review This part 	
-			String str = "https://"+ bucketName + ".s3." + region + ".amazonaws.com/" + "Document/Demo-" + contactId + ".pdf";
-			return str;// openWebpage(url.toURI(str));
+
+			S3Object s3object = s3Client.getObject(new GetObjectRequest(bucketName, key));
+
+			Object[] obj = clone(s3object.getObjectContent());
+
+			InputStream clone = (InputStream) obj[0];
+
+			InputStreamResource resource = new InputStreamResource(clone);
+
+			long contentLength = (long) obj[1];
+
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.add("content-disposition", "inline; filename=" + fileName);
+			return ResponseEntity.ok().headers(httpHeaders).contentLength(contentLength)
+					.contentType(MediaType.parseMediaType("application/pdf")).body(resource);
+
 		} catch (Exception e) {
 			System.err.println(e);
 		}
-			
-		return "Please wait while doc is loading";
+
+		return null;
 	}
 
+	private Object[] clone(InputStream inputStream) {
+
+		long size = 0;
+
+		try {
+			inputStream.mark(0);
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+			byte[] buffer = new byte[1024];
+			int readLength = 0;
+			while ((readLength = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, readLength);
+
+				size += readLength;
+			}
+
+			outputStream.flush();
+			return new Object[] { new ByteArrayInputStream(outputStream.toByteArray()), size };
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
 }
